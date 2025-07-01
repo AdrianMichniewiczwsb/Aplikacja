126d6e8e3f0ad79b62b47057ae1a87f0e2aa7b7e
Adrian Michniewicz <adrian.michniewicz13@gmail.com>
2025-07-01 21:09:11 +0200

    Prosty program z menu nawigacyjnym

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== MENU NAWIGACYJNE ===");
+        System.out.println("1. Strona główna");
+        System.out.println("2. O nas");
+        System.out.println("3. Kontakt");
+        System.out.println("=========================");
+    }
+}
