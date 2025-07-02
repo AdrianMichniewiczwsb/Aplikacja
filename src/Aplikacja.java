520f23c0ee95fb0dec23255e367135fb188f9048
Adrian Michniewicz <adrian.michniewicz13@gmail.com>
2025-07-02 19:04:04 +0200

    feat: dodano opcję mnożenia w menu kalkulatora

---
+++ src/Aplikacja.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Aplikacja {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== MENU NAWIGACYJNE ===");
+        System.out.println("1. Kalkulator dodawania");
+        System.out.println("2. Kalkulator mnożenia");
+        System.out.println("3. Wyjście");
+        System.out.print("Wybierz opcję: ");
+        int wybor = scanner.nextInt();
+
+        if (wybor == 1) {
+            System.out.print("Podaj pierwszą liczbę: ");
+            int a = scanner.nextInt();
+
+            System.out.print("Podaj drugą liczbę: ");
+            int b = scanner.nextInt();
+
+            int suma = a + b;
+            System.out.println("Suma " + a + " + " + b + " = " + suma);
+        } else if (wybor == 2) {
+            System.out.print("Podaj pierwszą liczbę: ");
+            int a = scanner.nextInt();
+
+            System.out.print("Podaj drugą liczbę: ");
+            int b = scanner.nextInt();
+
+            int wynik = a * b;
+            System.out.println("Wynik mnożenia: " + wynik);
+        } else if (wybor == 3) {
+            System.out.println("Do widzenia!");
+        } else {
+            System.out.println("Nieprawidłowa opcja, spróbuj ponownie.");
+        }
+
+        scanner.close();
+    }
+}
